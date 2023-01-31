edf267f17fbf7693d6b95569485992d5eb741471
Kareem Morsy <kreem.morsy@hotmail.com>
2023-01-31 23:48:39 +0200

    [GP-32] Rewrite slate types

--- src/pages/editor/components/slate/slate-types.d.ts
+++ src/pages/editor/components/slate/slate-types.d.ts
@@ -6,77 +6,16 @@ import { ReactEditor } from 'slate-react';
 import { HistoryEditor } from 'slate-history';
 import { PagedEditor } from './withPages';
 
-export type BlockQuoteElement = {
-  type: 'block-quote'
-  align?: string
-  children: Descendant[]
-}
+/**
+ * A top level element that contains all the elements in the document.
+ */
+export type Element =
+    | PageElement
+    | InPageElement
 
-export type BulletedListElement = {
-  type: 'bulleted-list'
-  align?: string
-  children: Descendant[]
-}
-
-export type CheckListItemElement = {
-  type: 'check-list-item'
-  checked: boolean
-  children: Descendant[]
-}
-
-export type EditableVoidElement = {
-  type: 'editable-void'
-  children: EmptyText[]
-}
-
-export type HeadingElement = {
-  type: 'heading'
-  align?: string
-  children: Descendant[]
-}
-
-export type HeadingTwoElement = {
-  type: 'heading-two'
-  align?: string
-  children: Descendant[]
-}
-
-export type ImageElement = {
-  type: 'image'
-  url: string
-  children: EmptyText[]
-}
-
-export type LinkElement = { type: 'link'; url: string; children: Descendant[] }
-
-export type ButtonElement = { type: 'button'; children: Descendant[] }
-
-export type ListItemElement = { type: 'list-item'; children: Descendant[] }
-
-export type ParagraphElement = {
-  type: 'paragraph'
-  align?: string
-  children: Descendant[]
-}
-
-export type TableElement = { type: 'table'; children: TableRow[] }
-
-export type TableCellElement = { type: 'table-cell'; children: CustomText[] }
-
-export type TableRowElement = { type: 'table-row'; children: TableCell[] }
-
-export type TitleElement = { type: 'title'; children: Descendant[] }
-
-export type VideoElement = { type: 'video'; url: string; children: EmptyText[] }
-
-export type PageElement = {
-  type: 'page';
-  children: Descendant[];
-  width: number;
-  height: number;
-  padding: PaddingProps;
-}
-
+/**
+ * Padding container.
+ */
 export interface PaddingProps {
   left?: number;
   right?: number;
@@ -84,34 +23,112 @@ export interface PaddingProps {
   bottom?: number;
 }
 
-type CustomElement =
-  | BlockQuoteElement
-  | BulletedListElement
-  | CheckListItemElement
-  | EditableVoidElement
-  | HeadingElement
-  | HeadingTwoElement
-  | ImageElement
-  | LinkElement
-  | ButtonElement
-  | ListItemElement
-  | ParagraphElement
-  | TableElement
-  | TableRowElement
-  | TableCellElement
-  | TitleElement
-  | VideoElement
-  | PageElement
+/**
+ * A page element is a top-level element that contains all the elements on a page.
+ */
+export interface PageElement {
+  type: 'page';
+  children: InPageDescendant[];
+  /**
+   * The width of the page in points.
+   */
+  width: number;
+  /**
+   * The height of the page in points.
+   */
+  height: number;
+  /**
+   * The padding of the page in points.
+   */
+  padding: PaddingProps;
+}
 
-export type CustomText = {
+/**
+ * A generic element that can be placed on a page.
+ */
+export type InPageElement =
+    | BlockQuoteElement
+    | BulletedListElement
+    | HeadingElement
+    | ImageElement
+    | ParagraphElement
+    | TableElement
+    | TableCellElement
+    | TableRowElement
+
+/**
+ * Any descendant that can be placed on a page.
+ */
+export type InPageDescendant = InPageElement | Text
+
+/**
+ * Generic element interface.
+ */
+export interface IInPageElement {
+  type: string,
+  /**
+   * The alignment of the element.
+   */
+  align?: string,
+  children: InPageDescendant[]
+}
+
+export interface BlockQuoteElement extends IInPageElement {
+  type: 'block-quote'
+}
+
+export interface BulletedListElement extends IInPageElement {
+  type: 'bulleted-list'
+}
+
+export interface HeadingElement extends IInPageElement {
+  type: 'heading',
+  /**
+   * The level of the heading.
+   */
+  level: 1 | 2 | 3 | 4 | 5 | 6
+}
+
+export interface ImageElement extends IInPageElement {
+  type: 'image'
+  /**
+   * The URL of the image.
+   */
+  url: string
+  children: EmptyText[]
+}
+
+export interface ParagraphElement extends IInPageElement {
+  type: 'paragraph'
+}
+
+export interface TableElement extends IInPageElement {
+  type: 'table';
+  children: TableRowElement[]
+}
+
+export interface TableCellElement extends IInPageElement {
+  type: 'table-cell';
+  children: Text[]
+  }
+
+export interface TableRowElement extends IInPageElement {
+  type: 'table-row';
+  children: TableCellElement[]
+}
+
+export interface Text {
+  text: string
   bold?: boolean
   italic?: boolean
   code?: boolean
-  text: string
 }
 
-export type EmptyText = {
-  text: string
+export interface EmptyText {
+  text: "",
+  bold?: false,
+  italic?: false,
+  code?: false
 }
 
 export type CustomEditor = BaseEditor & ReactEditor & HistoryEditor & PagedEditor
@@ -119,8 +136,8 @@ export type CustomEditor = BaseEditor & ReactEditor & HistoryEditor & PagedEdito
 declare module 'slate' {
   interface CustomTypes {
     Editor: CustomEditor
-    Element: CustomElement
-    Text: CustomText | EmptyText
+    Element: Element
+    Text: Text | EmptyText
   }
 }
 
@@ -130,7 +147,7 @@ declare global {
   }
 }
 
-interface ElementAttributes {
+export type ElementAttributes = {
   'data-slate-node': 'element';
   'data-slate-inline'?: true;
   'data-slate-void'?: true;
