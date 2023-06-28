3f97570a9c52972eb1e5b300e7a7998780aa4adb
Mostafa Morsy <mostafamorsy22@gmail.com>
2023-06-28 04:34:47 +0300

    updated states

--- src/views/EditExam.tsx
+++ src/views/EditExam.tsx
@@ -1,7 +1,20 @@
 import { Container } from "@mui/system";
 import ModelAnswerSegmenter from "../components/ModelAnswerSegmenter/ModelAnswerSegmenter";
-import { Button, ButtonGroup, Card, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, TextField, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  LinearProgress,
+  TextField,
+  Typography
+} from "@mui/material";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Question } from "../entities/Question";
 import { ModelAnswerSegment } from "../entities/ModelAnswerTypes";
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -11,49 +24,67 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Exam } from "../entities/Exam";
 import EditIcon from '@mui/icons-material/Edit';
 import CheckIcon from '@mui/icons-material/Check';
+import When from "../components/When";
 
 const EditExam = (props: { id: string }) => {
 
-  const editingExam: Exam = {
-    id: props.id,
-    name: 'English Exam',
-    mode: 'editing',
-    questions: [
-      {
-        title: "tst1",
-        modelAnswer: {
-          body: "testttttt herereere 1jiuo3242",
-        },
-      },
-      {
-        title: "tst2",
-        modelAnswer: {
-          body: "testttttt herereere 1jiuo3242",
-        },
-      },
-      {
-        title: "tst3",
-        modelAnswer: {
-          body: "testttttt herereere 1jiuo3242",
-        },
-      },
-    ]
-  };
 
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const [deleteDialogIndex, setDeleteDialogIndex] = useState<number>(-1);
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [exam, setExam] = useState<Exam>(editingExam);
+  const [exam, setExam] = useState<Exam>({
+    id: '',
+    mode: 'editing',
+    questions: [] as Question[],
+    name: ''
+  });
   const [editNameMode, setEditNameMode] = useState(false);
-  const [examName, setExamName] = useState(exam.name);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    console.log(questions);
-  }, [questions]);
-
-  useEffect(() => {
-    setQuestions(exam.questions);
+  const questions = useMemo(() => exam.questions, [exam]);
+  const setQuestions = useCallback((questions: Question[]) => {
+    setExam({...exam, questions});
   }, [exam]);
+
+  const examName = useMemo(() => exam.name, [exam]);
+  const setExamName = useCallback((name: string) => {
+    setExam({...exam, name});
+  }, [exam]);
+
+  useEffect(() => {
+
+    const fetchExam = async () => {
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setExam({
+        id: props.id,
+        name: 'English Exam',
+        mode: 'editing',
+        questions: [
+          {
+            title: "tst1",
+            modelAnswer: {
+              body: "testttttt herereere 1jiuo3242"
+            }
+          },
+          {
+            title: "tst2",
+            modelAnswer: {
+              body: "testttttt herereere 1jiuo3242"
+            }
+          },
+          {
+            title: "tst3",
+            modelAnswer: {
+              body: "testttttt herereere 1jiuo3242"
+            }
+          }
+        ]
+      });
+    };
+    setLoading(true);
+
+    fetchExam().then(() => (setLoading(false)));
+  }, []);
 
   const setSegments = (index: number) => (segements: ModelAnswerSegment[]) => {
     const newQuestions = [...questions];
@@ -100,8 +131,8 @@ const EditExam = (props: { id: string }) => {
     newQuestions.splice(index + 1, 0, {
       title: '',
       modelAnswer: {
-        body: '',
-      },
+        body: ''
+      }
     });
     setQuestions(newQuestions);
   };
@@ -137,27 +168,30 @@ const EditExam = (props: { id: string }) => {
   return (
     <div className='bg-gray-200/5'>
       <Container className='p-4 mt-4'>
+        <When isTrue={loading}>
+          <LinearProgress />
+        </When>
         <div className='flex flex-col gap-8'>
           <div className='flex'>
             <Card className='p-4 w-full'>
               {editNameMode ?
                 <div className='flex'>
-                  <TextField id="standard-basic" value={examName} onChange={handleEditingExamName} variant="standard" />
-                  <IconButton className='mx-1' size="large" onClick={disableEditingNameMode}>
+                  <TextField id='standard-basic' value={examName} onChange={handleEditingExamName} variant='standard' />
+                  <IconButton className='mx-1' size='large' onClick={disableEditingNameMode}>
                     <CheckIcon />
                   </IconButton>
                 </div> :
                 <div className='flex flex-wrap items-center'>
                   <Typography className='h-fit' variant='h5' >{examName}</Typography>
-                  <div className="mx-1">
-                    <IconButton size="small" onClick={enableEditingNameMode}>
+                  <div className='mx-1'>
+                    <IconButton size='small' onClick={enableEditingNameMode}>
                       <EditIcon />
                     </IconButton>
                   </div>
                 </div>}
             </Card>
-            <ButtonGroup className='my-3 mx-1' orientation="vertical">
-              <IconButton onClick={handleAddQuestion(-1)} size="large">
+            <ButtonGroup className='my-3 mx-1' orientation='vertical'>
+              <IconButton onClick={handleAddQuestion(-1)} size='large'>
                 <AddCircleIcon />
               </IconButton>
             </ButtonGroup>
@@ -165,7 +199,7 @@ const EditExam = (props: { id: string }) => {
           {questions.map((question, index) => (
             <div key={index} className='flex'>
               <Card className='p-6 w-full'>
-                <div className="flex flex-wrap">
+                <div className='flex flex-wrap'>
                   <Typography className='h-fit' variant='h5' >Question {index + 1}</Typography>
                 </div>
                 <TextField
@@ -181,30 +215,31 @@ const EditExam = (props: { id: string }) => {
                   sx={{ backgroundColor: '#FFFFFF', width: '100%', borderRadius: 2 }}
                 />
                 <div className='my-8'>
-                  <ModelAnswerSegmenter rows={8} modelAnswer={question.modelAnswer.body} setModelAnswer={setModelAnswer(index)}
+                  <ModelAnswerSegmenter rows={8}
+                    modelAnswer={question.modelAnswer.body} setModelAnswer={setModelAnswer(index)}
                     segments={question.modelAnswer.segements} setSegments={setSegments(index)}
                     mode={question.modelAnswer.mode} setMode={setMode(index)} />
                 </div>
               </Card>
-              <ButtonGroup className='my-8 mx-1' orientation="vertical">
-                <IconButton onClick={handleAddQuestion(index)} size="large">
+              <ButtonGroup className='my-8 mx-1' orientation='vertical'>
+                <IconButton onClick={handleAddQuestion(index)} size='large'>
                   <AddCircleIcon />
                 </IconButton>
-                <IconButton onClick={handleOpenDeleteDialog(index)} aria-label="delete" size="large">
-                  <DeleteIcon fontSize="inherit" />
+                <IconButton onClick={handleOpenDeleteDialog(index)} aria-label='delete' size='large'>
+                  <DeleteIcon fontSize='inherit' />
                 </IconButton>
                 {index > 0 &&
-                  <IconButton onClick={handleMoveUpQuestion(index)} size="large">
+                  <IconButton onClick={handleMoveUpQuestion(index)} size='large'>
                     <KeyboardArrowUpIcon />
                   </IconButton>}
                 {index + 1 < questions.length &&
-                  <IconButton onClick={handleMoveDownQuestion(index)} size="large">
+                  <IconButton onClick={handleMoveDownQuestion(index)} size='large'>
                     <KeyboardArrowDownIcon />
                   </IconButton>}
               </ButtonGroup>
             </div>
           ))}
-          <div className="flex justify-end px-14 pb-12">
+          <div className='flex justify-end px-14 pb-12'>
             <Button className='w-32' color='primary' variant='contained'>Save</Button>
           </div>
         </div>
