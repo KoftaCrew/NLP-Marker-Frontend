b0676c3a5ecc28882173f462d2b67e28ec7d80a0
Mostafa Morsy <mostafamorsy22@gmail.com>
2023-07-02 19:40:22 +0300

    removed StudentAnswer type

--- src/entities/StudentAnswersTypes.d.ts
+++ src/entities/StudentAnswersTypes.d.ts
@@ -1,15 +1,6 @@
-import { ReactComponentElement } from "react";
-
 export type StudentAnswersProps = {
   examId: string;
 };
-
-export type StudentAnswer = {
-  name: string;
-  answers: {
-    questionTitle: string,
-    insights: ReactComponentElement<InsightsViewer>}[];
-}
 
 export type Student = {
   name: string;
